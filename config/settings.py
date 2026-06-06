7d5578d3cab92d41840eb6e0bce15df05454b7ee
unknown <santiagocolimba10@gmail.com>
2026-06-06 16:07:50 -0500

    fix: add default values to DB config for CI compatibility

--- config/settings.py
+++ config/settings.py
@@ -53,11 +53,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     config('DB_NAME'),
-        'USER':     config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':     config('DB_HOST', default='localhost'),
-        'PORT':     config('DB_PORT', default='5432'),
+        'NAME':     config('DB_NAME',     default='titotech'),
+        'USER':     config('DB_USER',     default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST':     config('DB_HOST',     default='localhost'),
+        'PORT':     config('DB_PORT',     default='5432'),
         'TEST': {
             'NAME': config('TEST_DB_NAME', default='titotech_test_db'),
         },
